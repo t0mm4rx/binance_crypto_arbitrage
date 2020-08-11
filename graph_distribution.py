018d0f4ed726b879b5863c6c006f8ad9cf501047
Tom Marx <tom.marx@hotmail.fr>
2020-08-11 07:58:39 +0200

    Distribution plotting

---
+++ graph_distribution.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+lines = []
+values = []
+
+def process_percentage(n):
+	n = n.replace('%', '')
+	n = n.replace('\n', '')
+	n = float(n)
+	return n
+
+with open('logs.txt', 'r') as file:
+	lines = file.readlines()
+
+for line in lines:
+	split1 = line.split(':')
+	if (len(split1) == 4):
+		split2 = split1[3].split('/')
+		if (len(split2) == 2):
+			values.append(process_percentage(split2[0]))
+			values.append(process_percentage(split2[1]))
+
+plt.hist(values, range=[-1, 0], bins=40)
+# plt.show()
+plt.savefig('distrib.png')
